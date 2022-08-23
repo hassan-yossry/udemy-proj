9c0020fd7fac207bfd5038ab0a43df61a4e7c28f
Hassan Yossry <hassan.yossry_pg@alexu.edu.eg>
2022-08-23 11:46:50 +0200

    done a last resort prettier and lint pass

--- src/tests/utilSpec.ts
+++ src/tests/utilSpec.ts
@@ -1,40 +1,47 @@
-import {validateImageName,initMaps,validateHeightWidth} from '../routes/api/utilities'
+import {
+  validateImageName,
+  initMaps,
+  validateHeightWidth
+} from '../routes/api/utilities';
 import fs from 'fs';
 import path from 'path';
-describe("this is a test for helper for validateImageName",()=>{
-    
-    it("start and test if validate correct input",()=>{
-        initMaps()
-    const files = fs.readdirSync(path.join(__dirname,'../../images'))
-        for(let file of files){
-            expect(validateImageName(file.slice(0,-4))).toBeTrue()
-        }
-    })
-    it('Throws error for an invalid name',()=>{
-        expect(()=>validateImageName('noimagevcanhavethisname')).toThrowError('Enter a valid image name')
-    })
-    
-    
-})
+describe('this is a test for helper for validateImageName', () => {
+  it('start and test if validate correct input', () => {
+    initMaps();
+    const files = fs.readdirSync(path.join(__dirname, '../../images'));
+    for (const file of files) {
+      expect(validateImageName(file.slice(0, -4))).toBeTrue();
+    }
+  });
+  it('Throws error for an invalid name', () => {
+    expect(() => validateImageName('noimagevcanhavethisname')).toThrowError(
+      'Enter a valid image name'
+    );
+  });
+});
 
-
-describe("this is a test for helper for validateHeightWidth",()=>{
-    
-    it("works for valid values",()=>{
-        expect(validateHeightWidth('20','100')).toBeTrue();
-    })
-    it("height is numerical",()=>{
-        expect(()=>validateHeightWidth('nn','100')).toThrowError('Height and width must be numbers');
-    })
-    it("width is numerical",()=>{
-        expect(()=>validateHeightWidth('200','cc')).toThrowError('Height and width must be numbers');
-    })
-    it("height is greater than zero",()=>{
-        expect(()=>validateHeightWidth('-2','100')).toThrowError('Enter a height and width value greater than zero');
-    })
-    it("width is greater than zero",()=>{
-        expect(()=>validateHeightWidth('3','0')).toThrowError('Enter a height and width value greater than zero');
-    })
-    
-})
-
+describe('this is a test for helper for validateHeightWidth', () => {
+  it('works for valid values', () => {
+    expect(validateHeightWidth('20', '100')).toBeTrue();
+  });
+  it('height is numerical', () => {
+    expect(() => validateHeightWidth('nn', '100')).toThrowError(
+      'Height and width must be numbers'
+    );
+  });
+  it('width is numerical', () => {
+    expect(() => validateHeightWidth('200', 'cc')).toThrowError(
+      'Height and width must be numbers'
+    );
+  });
+  it('height is greater than zero', () => {
+    expect(() => validateHeightWidth('-2', '100')).toThrowError(
+      'Enter a height and width value greater than zero'
+    );
+  });
+  it('width is greater than zero', () => {
+    expect(() => validateHeightWidth('3', '0')).toThrowError(
+      'Enter a height and width value greater than zero'
+    );
+  });
+});
